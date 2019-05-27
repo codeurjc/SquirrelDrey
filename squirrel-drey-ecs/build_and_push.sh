4854f302cb63d769892da002352165be5016c660
Fede Diaz <nordri@gmail.com>
2019-05-27 15:36:49 +0200

    Client registry

--- squirrel-drey-ecs/build_and_push.sh
+++ squirrel-drey-ecs/build_and_push.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 
 cp ../squirrel-drey-sampleapp/target/squirrel-drey-sampleapp*.jar .
 
-docker build -t 849201093595.dkr.ecr.eu-west-1.amazonaws.com/squirreldrey:latest .
-docker push 849201093595.dkr.ecr.eu-west-1.amazonaws.com/squirreldrey:latest
+docker build -t 093555767787.dkr.ecr.eu-west-1.amazonaws.com/squirreldrey:sampleapp .
+docker push 093555767787.dkr.ecr.eu-west-1.amazonaws.com/squirreldrey:sampleapp
