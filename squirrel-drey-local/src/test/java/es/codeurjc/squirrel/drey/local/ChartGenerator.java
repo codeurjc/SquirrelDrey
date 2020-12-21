2971f38b47f382b9351038bb648dbd4e5c85fccd
cruizba <charlesrb94@outlook.com>
2020-12-21 18:53:00 +0100

    Add Simulation result charts generator

--- squirrel-drey-local/src/test/java/es/codeurjc/squirrel/drey/local/ChartGenerator.java
+++ squirrel-drey-local/src/test/java/es/codeurjc/squirrel/drey/local/ChartGenerator.java
@@ -1,2 +1,101 @@
-package es.codeurjc.squirrel.drey.local;public class ChartGenerator {
+package es.codeurjc.squirrel.drey.local;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ChartGenerator {
+
+    Path resourceDirectory = Paths.get("src","test","resources");
+    private final String TEMPLATE_RESOURCE_FILE = "chart_template.html";
+
+    public enum TemplateKeys {
+
+        LABELS_LIST("labelsList"),
+        HIGH_PRIORITY_TASK("highPriorityTasks"),
+        LOW_PRIORITY_TASKS("lowPriorityTasks"),
+        TOTAL_TASKS("totalTasks"),
+        RUNNING_WORKERS("runningWorkers"),
+        LAUNCHING_WORKERS("launchingWorkers"),
+        TERMINATING_WORKERS("terminatingWorkers"),
+        DISCONNECTED_WORKERS("disconnectedWorkers"),
+        IDLE_WORKERS("idleWorkers"),
+        MIN_WORKERS_LIST("minWorkersList"),
+        MAX_WORKERS_LIST("maxWorkersList"),
+        MIN_IDDLE_WORKERS("minIdleWorkers");
+
+        private String varDeclaration;
+
+        TemplateKeys(String varDeclaration) {
+            this.varDeclaration = varDeclaration;
+        }
+
+        public String getJSCode(SimulationResult simulationResult) {
+            List<?> resultList = simulationResult.getResultByTemplateKeys(this);
+            if (this == TemplateKeys.LABELS_LIST) {
+                String JSCode = "var " + this.varDeclaration + "=[";
+                Iterator<?> iterator = resultList.iterator();
+                while(iterator.hasNext()) {
+                    JSCode += "\"" + iterator.next() + "\"" + (iterator.hasNext() ? "," : "");
+                }
+                JSCode += "];\n";
+                return JSCode;
+            }
+            return "var " + this.varDeclaration + "=" + resultList.toString() + ";\n";
+        }
+    }
+
+    private String getResourceFileAsString(String fileName) throws IOException {
+        String absolutePathToFile = resourceDirectory.toAbsolutePath() + "/" + fileName;
+        File file = new File(absolutePathToFile);
+        try (InputStream is = new FileInputStream(file)) {
+            if (is == null) return null;
+            try (InputStreamReader isr = new InputStreamReader(is);
+                 BufferedReader reader = new BufferedReader(isr)) {
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+        }
+    }
+
+    private void writeStringAsFile(String text, String outputDir) throws IOException {
+        if (outputDir == null) {
+            // Search for non existing file if not specified output dir
+            String prefixSimulationFileName = "simulation_";
+            String suffixSimulationFileName = ".html";
+            int index = 1;
+            boolean fileNotExistingFound = false;
+            while(!fileNotExistingFound) {
+                String absolutePathToFile = resourceDirectory.toAbsolutePath() + "/" + prefixSimulationFileName + index + suffixSimulationFileName;
+                File f = new File(absolutePathToFile);
+                if (!f.exists()) {
+                    fileNotExistingFound = true;
+                    outputDir = absolutePathToFile;
+                } else {
+                    index++;
+                }
+            }
+        }
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(outputDir), "utf-8"))) {
+            writer.write(text);
+        }
+    }
+
+    public void saveSimulationResult(SimulationResult simulationResult) throws IOException {
+        this.saveSimulationResult(simulationResult, null);
+    }
+
+    public void saveSimulationResult(SimulationResult simulationResult, String outputDir) throws IOException {
+        String chartTemplate = getResourceFileAsString(this.TEMPLATE_RESOURCE_FILE);
+        StringBuilder dataSetJS = new StringBuilder();
+        for (TemplateKeys templateKey : TemplateKeys.values()) {
+            dataSetJS.append(templateKey.getJSCode(simulationResult));
+        }
+
+        writeStringAsFile(chartTemplate.replace("{{DATASET}}", dataSetJS), outputDir);
+    }
 }
---
+++ squirrel-drey-local/src/test/java/es/codeurjc/squirrel/drey/local/SimulationResult.java
@@ -0,0 +1,87 @@
+package es.codeurjc.squirrel.drey.local;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimulationResult {
+
+    private List<String> labelsList = new ArrayList<>();
+    private List<Integer> highPriorityTasks = new ArrayList<>();
+    private List<Integer> lowPriorityTasks = new ArrayList<>();
+    private List<Integer> totalTasks = new ArrayList<>();
+    private List<Integer> runningWorkers = new ArrayList<>();
+    private List<Integer> launchingWorkers = new ArrayList<>();
+    private List<Integer> terminatingWorkers = new ArrayList<>();
+    private List<Integer> disconnectedWorkers = new ArrayList<>();
+    private List<Integer> idleWorkers = new ArrayList<>();
+    private List<Integer> minWorkersList = new ArrayList<>();
+    private List<Integer> maxWorkersList = new ArrayList<>();
+    private List<Integer> minIdleWorkers = new ArrayList<>();
+
+    public List<String> getLabelsList() {
+        return labelsList;
+    }
+
+    public List<Integer> getHighPriorityTasks() {
+        return highPriorityTasks;
+    }
+
+    public List<Integer> getLowPriorityTasks() {
+        return lowPriorityTasks;
+    }
+
+    public List<Integer> getTotalTasks() {
+        return totalTasks;
+    }
+
+    public List<Integer> getRunningWorkers() {
+        return runningWorkers;
+    }
+
+    public List<Integer> getLaunchingWorkers() {
+        return launchingWorkers;
+    }
+
+    public List<Integer> getTerminatingWorkers() {
+        return terminatingWorkers;
+    }
+
+    public List<Integer> getDisconnectedWorkers() {
+        return disconnectedWorkers;
+    }
+
+    public List<Integer> getIdleWorkers() {
+        return idleWorkers;
+    }
+
+    public List<Integer> getMinWorkersList() {
+        return minWorkersList;
+    }
+
+    public List<Integer> getMaxWorkersList() {
+        return maxWorkersList;
+    }
+
+    public List<Integer> getMinIdleWorkers() {
+        return minIdleWorkers;
+    }
+
+
+    protected List<?> getResultByTemplateKeys(ChartGenerator.TemplateKeys templateKey) {
+        switch (templateKey) {
+            case LABELS_LIST: return this.labelsList;
+            case HIGH_PRIORITY_TASK: return this.highPriorityTasks;
+            case LOW_PRIORITY_TASKS: return this.lowPriorityTasks;
+            case TOTAL_TASKS: return this.totalTasks;
+            case RUNNING_WORKERS: return this.runningWorkers;
+            case LAUNCHING_WORKERS: return this.launchingWorkers;
+            case TERMINATING_WORKERS: return this.terminatingWorkers;
+            case DISCONNECTED_WORKERS: return this.disconnectedWorkers;
+            case IDLE_WORKERS: return this.idleWorkers;
+            case MIN_WORKERS_LIST: return this.minWorkersList;
+            case MAX_WORKERS_LIST: return this.maxWorkersList;
+            case MIN_IDDLE_WORKERS: return this.minIdleWorkers;
+            default: throw new IllegalArgumentException("Template key is not recognised");
+        }
+    }
+}
